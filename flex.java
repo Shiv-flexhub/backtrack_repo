a5af3142d7a780abef9cb8a741a48cec056863b3
debz-g <debz.exe@gmail.com>
2021-02-21 14:08:10 +0530

    Added a print statement

--- flex.java
+++ flex.java
@@ -8,6 +8,7 @@ System.out.println("Dum nhi hai laand me chale hai ghusane gaand me");
 System.out.println("pedd pe baitha kaal kauwaa uski kaali pooch aurjo na boole sarararaaa uski maa ki choot....toh bolo sarararaaaa!!!");
 System.out.println("Dayanada ki amma bole bhaag bhosada bhagg jaaa!!!");
 System.out.println("At last, maa chudao!!@@!!");
+System.out.println("Duniya Madarchod Hai");
 }
 }
 
